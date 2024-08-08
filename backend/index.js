8c7d9c1bb45fbc6318126773e19548445ea28bda
Darpan Neve <darpanneve3@gmail.com>
2024-08-08 17:23:51 +0530

    minor changes in dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const router = app.Router();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 app.listen(PORT, "0.0.0.0", () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port ' + PORT);
 }); 
